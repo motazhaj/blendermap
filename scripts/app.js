6a1c8a430f692c2ff68b24afd3836a6260833d24
motazhaj <motazhajm@gmail.com>
2023-09-03 12:41:06 +0300

    Remove not usefull comments

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,6 @@ const feedbackText = document.getElementById("feedback");
 const feedbackCounterValue = document.getElementById("feedbackCounter");
 
 function calculateFeedbackCounter() {
-  //get feedback text current length
-  // when limit - current = 10 warning
-  // when limit = current stop input
   const limit = feedbackText.getAttribute("maxLength");
 
   const textCounter = feedbackText.textLength;
